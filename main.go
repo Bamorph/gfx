478bf06472058b551c4e40f06e03eb234e031cec
Bamorph <142395807+Bamorph@users.noreply.github.com>
2023-12-11 16:26:44 +1100

    Update main.go
    updates

--- main.go
+++ main.go
@@ -7,15 +7,25 @@ import (
 	"regexp"
 )
 
-func findFilesByExtension(root string, ext string, pattern *regexp.Regexp) {
+func findFilesByExtensions(root string, exts []string) {
+	extPatterns := make([]*regexp.Regexp, len(exts))
+	for i, ext := range exts {
+		extPatterns[i] = regexp.MustCompile("\\." + ext + "$")
+	}
+
 	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			fmt.Println(err)
 			return nil
 		}
 
-		if !info.IsDir() && pattern.MatchString(info.Name()) {
-			fmt.Println(path)
+		if !info.IsDir() {
+			for _, pattern := range extPatterns {
+				if pattern.MatchString(info.Name()) {
+					fmt.Println(path)
+					break
+				}
+			}
 		}
 
 		return nil
@@ -24,13 +34,12 @@ func findFilesByExtension(root string, ext string, pattern *regexp.Regexp) {
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: go run main.go <file_extension>")
+		fmt.Println("Usage: go run main.go <file_extension1> [<file_extension2> ...]")
 		os.Exit(1)
 	}
 
 	root := "." // You can change this to the desired directory
-	ext := os.Args[1]
-	extPattern := regexp.MustCompile("\\." + ext + "$")
+	exts := os.Args[1:]
 
-	findFilesByExtension(root, ext, extPattern)
+	findFilesByExtensions(root, exts)
 }
